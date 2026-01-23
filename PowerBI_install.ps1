99fc4234e0ed02d545d07b4c15748f3124f9867f
Tobias Martinsson <tm@abobo.se>
2026-01-23 14:03:25 +0100

    Update PowerBI_install.ps1
    Maybe working.

--- PowerBI_install.ps1
+++ PowerBI_install.ps1
@@ -1,23 +1,18 @@
-# Org Script from: https://community.fabric.microsoft.com/t5/Desktop/Install-Desktop-Power-BI-Using-PowerShell/td-p/3964041 Anonymous creatored the original script.
+# Org Script from: https://community.fabric.microsoft.com/t5/Desktop/Install-Desktop-Power-BI-Using-PowerShell/td-p/3964041 Anonymous is the creator of the original script.
 # Modified by: Tobias Martinsson
 # Date: 2025-01-19
 # Description: This script downloads and installs Power BI Desktop silently with additional parameters. And then cleans up the installer file.
 
 # Define the URL for downloading Power BI Desktop
-$url = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe"
-
+$Url = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe"
 # Specify the destination path for the downloaded installer
-$Destfolder = "C:\Temp\Downloads"
-$Destination = $Destfolder + "\PBIDesktop_x64.exe"
-
+$DestFolder = "C:\Temp\Downloads\"
+$Destination = $DestFolder + "PBIDesktop_x64.exe"
 # Download the Power BI Desktop installer
-Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path $Destination -Force )
-
-# Define the installation path (optional, change as needed)
-$installPath = "C:\Program Files\Power BI Desktop"
-
+Invoke-WebRequest -Uri $Url -OutFile (New-Item -Path $Destination -Force)
+# Define the installation arguments (optional, change as needed)
+$InstallArgs =  "/quiet ACCEPT_EULA=1 INSTALLDESKTOPSHORTCUT=0"
 # Start the silent installation process with additional parameters
-Start-Process -FilePath $Destination -ArgumentList "/quiet ACCEPT_EULA=1 INSTALLLOCATION=$installPath" -Wait -NoNewWindow
-
+Start-Process -FilePath $Destination -ArgumentList $InstallArgs -Wait -NoNewWindow
 # Cleanup - Remove the installer after installation
-Remove-Item -Path $Destfolder -Force
+Remove-Item -Path $DestFolder -Recurse -Force
